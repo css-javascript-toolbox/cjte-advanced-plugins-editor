ab04b31694982cc4855b95dd8325faa3660ea986
AHMeD <xpointer@hotmail.com>
2014-10-10 14:30:41 +0200

    = Core =
    + Hold edit file scroll position per file.
    + Support File and General Cooked propertiues
    + Interact with real extension interface through separated Interface Object

--- views/adveditor/public/js/dummycjtblock.js
+++ views/adveditor/public/js/dummycjtblock.js
@@ -15,12 +15,6 @@ var CJTBlockPluginBase;
 	CJTBlockPluginBase = function() {
 		
 		/**
-		* put your comment there...
-		* 
-		*/
-		var selectedThemeName;
-		
-		/**
 		* 
 		*/
 		this.load = function() {
@@ -38,7 +32,10 @@ var CJTBlockPluginBase;
 			var fileName = $('#template').find('input[name="file"]').val();
 			var fileExtension = fileName.substring(fileName.length - 3);
 			// Set options
-			this.editor.getSession().setMode('ace/mode/' + fileExtension);
+			var editorSession = this.editor.getSession();
+			editorSession.setMode('ace/mode/' + fileExtension);
+			// Editor default options.
+			this.editor.setOptions({showPrintMargin : false});
 			// Theme object
 			this.theme = {};
 			// Create Dummy Block AcEditor
@@ -57,32 +54,92 @@ var CJTBlockPluginBase;
 				/**
 				* 
 				*/
-				this.get = function(propName, defaultValue) {
-					// Initialize
-					value = undefined;
-					// Get property value
-					switch (propName) {
-						case 'editorTheme':
-							value = selectedThemeName ? selectedThemeName : defaultValue;
-						break;
-					}
+				this.get = function(propName, defaultValue, type) {
+					// Get property class type
+					type = (type !== undefined) ? type : 'getCookiePropertyName';
+					// Read cookies value
+					var cookieName = this[type](propName);
+					var cookiedValue = $.cookies.get(cookieName);
+					// Return cookies or default if no cookies available for theme
+					value = cookiedValue ? cookiedValue : defaultValue;
+					// Return value
 					return value;
 				};
 
 				/**
 				* 
 				*/
-				this.set = function(propName, value) {
-					// Get property value
-					switch (propName) {
-						case 'editorTheme':
-							selectedThemeName = value;
-						break;
-					}
+				this.getCookieFilePropertyValue = function(name, defaultValue) {
+					return this.get(name, defaultValue, 'getCookieFilePropertyName');
+				};
+				
+				/**
+				* 
+				*/
+				this.getCookieFilePropertyName = function(name) {
+					return this.getCookiePropertyName(name + '-' + fileName);
+				};
+		
+				/**
+				* 
+				*/
+				this.getCookiePropertyName = function(name) {
+					return 'CJTEAPEExtDef' + '-' + CJTEAPEExtDef.getName() + '-' + name; 
+				};
+		
+				/**
+				* 
+				*/
+				this.getCookiePropertyValue = function(name, defaultValue) {
+					return this.get(name, defaultValue);
+				};
+
+				/**
+				* 
+				*/
+				this.getFileScrollPosition = function() {
+					return this.getCookieFilePropertyValue('scrollPosition', 0);
+				};
+				
+				/**
+				* 
+				*/
+				this.set = function(propName, value, type) {
+					// Get property class type
+					type = (type !== undefined) ? type : 'getCookiePropertyName';
+					// Set value
+					var cookieName = this[type](propName);
+					$.cookies.set(cookieName, value);
+					// Chain
+					return this;
+				};
+		
+				/**
+				* 
+				*/
+				this.setCookieFilePropertyValue = function(name, value) {
+					return this.set(name, value, 'getCookieFilePropertyName');
+				};
+
+				/**
+				* 
+				*/
+				this.setCookiePropertyValue = function(name, value) {
+					return this.set(name, value);
+				};
+				
+				/**
+				* 
+				*/
+				this.setFileScrollPosition = function(value)	{
+					return this.setCookieFilePropertyValue('scrollPosition', value);
 				};
 		
 			})(this);
 		
+			// Set scroll top
+			editorSession.setScrollTop(this.block.getFileScrollPosition());
+
 			// Create Editor toolbox markup
 			$('<div class="cjt-toolbox editor-toolbox"><div class="icons-group"></div></div>').insertBefore(editorElement);
 			
@@ -95,10 +152,14 @@ var CJTBlockPluginBase;
 			}).get(0).CJTToolBox;
 		
 			// Set fil content before form submission
-			$('#submit').click($.proxy(
+			$('#template').submit($.proxy(
 				function() {
+					// Get editor session
+					var session = this.editor.getSession();
 					// Fill regular editor
-					regularEditor.text(this.editor.getSession().getValue());
+					regularEditor.text(session.getValue());
+					// Hold scroll top position
+					this.block.setFileScrollPosition(session.getScrollTop());
 				}, this)
 			);
 		
